a33b031492fd7b376cd571478d902d614199274a
Dede Alamsyah <86579930+dedealamsyah@users.noreply.github.com>
2024-11-09 09:34:17 +0700

    first commit

---
+++ timmaker_ilkomif.py
@@ -0,0 +1,34 @@
+import random
+
+def input_members():
+    members = []
+    print("Kelompok Presentasi - ILKOMIF (SEM1) UNIPI Kamda Cisurupan, Garut ")
+    while True:
+        member = input("Nama Mahasiswa: ")
+        if member.lower() == 'done':
+            break
+        members.append(member)
+    return members
+
+def create_teams(members, num_teams):
+   
+    random.shuffle(members)
+    
+
+    teams = [[] for _ in range(num_teams)]
+    for i, member in enumerate(members):
+        teams[i % num_teams].append(member)
+    
+    return teams
+
+
+members = input_members()
+
+num_teams = int(input("Masukkan jumlah tim: "))
+
+
+teams = create_teams(members, num_teams)
+
+
+for i, team in enumerate(teams, 1):
+    print(f"Tim {i}: {', '.join(team)}")
